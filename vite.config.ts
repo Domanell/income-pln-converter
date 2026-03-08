d287f9b2577de25675fc0e96be0466bc3bc9fa94
Oleksandr Pavlii <pavliy.av@gmail.com>
2026-03-08 18:15:47 +0100

    Set base path based on command

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
-  base: '/income-pln-converter/',
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/income-pln-converter/' : '/',
   plugins: [react(), tailwindcss()],
-})
+}))
